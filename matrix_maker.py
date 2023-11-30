583cecb2c2f2e5027a484130ce46be4e87271914
hurricaneJoef <hurricane.joef@gmail.com>
2023-11-30 10:09:32 -0500

    added lower performance code

--- matrix_maker.py
+++ matrix_maker.py
@@ -19,7 +19,6 @@ def generate_page_list(db):
 def generate_match_list(pagelist,match_set):
     return [1*(page in match_set) for page in pagelist]
 
-
 def make_weighted_matrix(db,pagelist):
     size = [len(pagelist),len(pagelist)]
     print(size)
@@ -27,12 +26,26 @@ def make_weighted_matrix(db,pagelist):
     for i,key in enumerate(pagelist):
         match_set = db.links_from_page[key]
         row = generate_match_list(pagelist,match_set)
-        print(np.sum(row))
+        #print(np.sum(row))
         h[i] = (row/np.sum(row))
         
     #remove the diagonal
     np.fill_diagonal(h,0)
     return h
+
+def get_row_weights(db,page_list):
+    return np.array([np.sum(
+        generate_match_list(page_list,db.links_from_page[key])
+        ) for key in page_list])
+
+def multiply_by_H(db,page_list,v):
+    row_weights = get_row_weights(db,page_list).T
+    for i,key in enumerate(page_list):
+        match_set = db.links_to_page[key]
+        col = generate_match_list(page_list,match_set)
+        weighted_col = np.multiply(col,row_weights)
+        print(weighted_col)
+        v[i] = np.sum(np.multiply(weighted_col,row_weights.T))
 
 
 if __name__=="__main__":
